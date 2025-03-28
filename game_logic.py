5ba7919609666801552428e59a8548bf1ff50b89
dododdias <159502266+dododdias@users.noreply.github.com>
2025-03-27 21:58:10 -0500

    game_logic.py

--- game_logic.py
+++ game_logic.py
@@ -1,42 +1,39 @@
-class GameLogic:
-    def __init__(self, size, gamemode="simple"):
+class BaseGameLogic:
+    def __init__(self, size):
         self.size = size
-        self.gamemode = gamemode  # "simple" or "general"
         self.board = [['' for _ in range(size)] for _ in range(size)]
-        self.currentplayer = 'blue'  # 'blue' or 'red'
-        self.gameover = False
-        self.bluescore = 0
-        self.redscore = 0
+        self.current_player = 'blue'  # 'blue' or 'red'
+        self.game_over = False
+        self.blue_score = 0
+        self.red_score = 0
 
-    def makemove(self, row, col, piece):
-        # Try to make a move on the board. Output is True if move is valid, False if not.
-
+    def make_move(self, row, col, piece):
+        """
+        Attempts to make a move on the board.
+        Returns True if move is valid, False otherwise.
+        """
         if not self.is_valid_move(row, col):
             return False
 
         self.board[row][col] = piece
-        self.checksos(row, col, piece)
+        self.check_sos(row, col, piece)
         
-        if not self.gamemode == "general":
-            if self.bluescore > 0 or self.redscore > 0:
-                self.gameover = True
-        elif self.is_board_full():
-            self.gameover = True
-
-        if not self.gameover:
-            self.currentplayer = 'red' if self.currentplayer == 'blue' else 'blue'
+        self.check_game_over()
+        
+        if not self.game_over:
+            self.current_player = 'red' if self.current_player == 'blue' else 'blue'
         
         return True
 
     def is_valid_move(self, row, col):
-        #Check if is valid.
+        """Check if the move is valid."""
         return (0 <= row < self.size and 
                 0 <= col < self.size and 
                 self.board[row][col] == '' and 
-                not self.gameover)
+                not self.game_over)
 
-    def checksos(self, row, col, piece):
-        #check if creates SOS
+    def check_sos(self, row, col, piece):
+        """Check if the move creates an SOS."""
         directions = [
             (-1, -1), (-1, 0), (-1, 1),
             (0, -1),          (0, 1),
@@ -45,38 +42,58 @@ class GameLogic:
         
         for dx, dy in directions:
             if piece == 'S':
-                #Check for "SOS" 
+                # Check for "SOS" pattern
                 try:
                     if (self.board[row + dx][col + dy] == 'O' and 
                         self.board[row + 2*dx][col + 2*dy] == 'S'):
-                        if self.currentplayer == 'blue':
-                            self.bluescore += 1
+                        if self.current_player == 'blue':
+                            self.blue_score += 1
                         else:
-                            self.redscore += 1
+                            self.red_score += 1
                 except IndexError:
                     continue
             elif piece == 'O':
-                # Check for SOS with O in the middle
+                # Check for "SOS" pattern with O in the middle
                 try:
                     if (self.board[row - dx][col - dy] == 'S' and 
                         self.board[row + dx][col + dy] == 'S'):
-                        if self.currentplayer == 'blue':
-                            self.bluescore += 1
+                        if self.current_player == 'blue':
+                            self.blue_score += 1
                         else:
-                            self.redscore += 1
+                            self.red_score += 1
                 except IndexError:
                     continue
 
     def is_board_full(self):
-        #check if board is full
+        """Check if the board is full."""
         return all(all(cell != '' for cell in row) for row in self.board)
 
+    def check_game_over(self):
+        """Check if game should be over (to be implemented by subclasses)"""
+        raise NotImplementedError
+
     def get_winner(self):
-        #gets the winner
-        if not self.gameover:
+        """Returns the winner of the game."""
+        if not self.game_over:
             return None
-        if self.bluescore > self.redscore:
+        if self.blue_score > self.red_score:
             return 'blue'
-        elif self.redscore > self.bluescore:
+        elif self.red_score > self.blue_score:
             return 'red'
         return 'tie'
+
+
+class SimpleGameLogic(BaseGameLogic):
+    def check_game_over(self):
+        """Simple game ends when first SOS is created or board is full with no SOS"""
+        if self.blue_score > 0 or self.red_score > 0:
+            self.game_over = True
+        elif self.is_board_full():
+            self.game_over = True
+
+
+class GeneralGameLogic(BaseGameLogic):
+    def check_game_over(self):
+        """General game ends when board is full"""
+        if self.is_board_full():
+            self.game_over = True
